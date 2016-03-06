e1a4d8777054a37f4ff6e1e5732a1a03850b2951
Arjan El Fassed <arjan@openstate.eu>
2016-03-06 12:01:30 +0100

    Update graphic.js

--- dutcharmsexports/js/graphic.js
+++ dutcharmsexports/js/graphic.js
@@ -36,7 +36,7 @@ $(document).ready(function() {
     function writeTable(data){
         //select main div and put a table there
 		//use bootstrap css to customize table style: http://getbootstrap.com/css/#tables 
-        $('#graphic').html('<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered dt-responsive nowrap" id="mySelection"></table>');
+        $('#graphic').html('<table id="mySelection" class="display responsive nowrap" cellspacing="0" width="100%"></table>');
 
         //initilize the DataTable object and put settings in
         $("#mySelection").DataTable({
